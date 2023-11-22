0291277fd7e1dcf0bdc418c6c2ecbbec2487fe36
Jack Sinko <jack@roadburngames.com>
2023-11-23 09:36:36 +1030

    Add acceleration logic

--- Assets/_Gumball/Runtime/Scripts/Traffic/TrafficCar.cs
+++ Assets/_Gumball/Runtime/Scripts/Traffic/TrafficCar.cs
@@ -14,20 +14,32 @@ namespace Gumball
         private const float timeBetweenDelayedUpdates = 1;
         private const float carActivationRange = 100;
         private const float collisionRecoverDuration = 5;
-        
+        private const int movementTargetDistance = 3; //the number of spline samples
+
         [SerializeField] private Transform[] frontWheels;
         [SerializeField] private Transform[] wheels;
-        [SerializeField] private float wheelRotateSpeed = 10;
+        [SerializeField] private float wheelRotateSpeed = 85; //todo: adjust depending on the moveSpeed 
+        [Space(5)]
+        [SerializeField] private float maxSpeed = 10; //todo: remove and use the chunk's speed limit
+        [SerializeField] private float turnSpeed = 3; //todo: adjust depending on moveSpeed
+        [Space(5)]
+        [SerializeField] private float accelerationDuration = 3;
+        [SerializeField] private float decelerationDuration = 2;
         
-        [SerializeField] private float moveSpeed = 10; //todo: remove and use the chunk's speed limit
-        [SerializeField] private float turnSpeed = 10;
-
         [Header("Debugging")]
         [SerializeField] private bool debug;
         [SerializeField, ReadOnly] private bool isInitialised;
         [SerializeField, ReadOnly] private bool isFrozen;
         [SerializeField, ReadOnly] private bool isActivated = true;
         [SerializeField, ReadOnly] private Chunk currentChunk;
+        [Space(5)]
+        [SerializeField, ReadOnly] private bool isMoving;
+        [SerializeField, ReadOnly] private float speed;
+        [SerializeField, ReadOnly] private bool isAccelerating;
+        [SerializeField, ReadOnly] private float timeSinceAccelerating;
+        [SerializeField, ReadOnly] private bool isDecelerating;
+        [SerializeField, ReadOnly]  private float timeSinceDecelerating;
+        [Space(5)]
         [SerializeField, ReadOnly] private float timeSinceCollision;
         [SerializeField, ReadOnly] private bool inCollision;
 
@@ -80,13 +92,13 @@ namespace Gumball
         {
             currentLaneDistance = laneDistance;
         }
-        
+
         private void OnCollisionEnter(Collision collision)
         {
             if (!GameObjectLayers.TrafficCarCollisionLayers.ContainsLayer(collision.gameObject.layer))
                 return;
 
-            Debug.Log($"{gameObject.name} collided with {collision.gameObject.name}");
+            GlobalLoggers.TrafficLogger.Log($"{gameObject.name} collided with {collision.gameObject.name}");
 
             if (collisions.Count == 0)
             {
@@ -97,7 +109,7 @@ namespace Gumball
 
             if (collision.rigidbody.Equals(PlayerCarManager.Instance.CurrentCar.Rigidbody))
             {
-                Debug.Log($"Player hit {gameObject.name} at {collision.impulse.magnitude}m/s");
+                GlobalLoggers.TrafficLogger.Log($"Player hit {gameObject.name} at {collision.impulse.magnitude}m/s");
             }
         }
 
@@ -117,6 +129,7 @@ namespace Gumball
         private void OnCollisionStart()
         {
             inCollision = true;
+            OnStopMoving();
         }
         
         private void OnCollisionEnd()
@@ -198,6 +211,147 @@ namespace Gumball
             }
         }
 
+        private void OnStartMoving()
+        {
+            isMoving = true;
+            OnStartAccelerating();
+        }
+
+        private void OnStopMoving()
+        {
+            isMoving = false;
+            speed = 0;
+            OnStopAccelerating();
+            onStopDecelerating();
+        }
+        
+        private void OnStartAccelerating()
+        {
+            isAccelerating = true;
+            timeSinceAccelerating = 0;
+        }
+
+        private void OnAccelerate()
+        {
+            timeSinceAccelerating += Time.deltaTime;
+        }
+
+        /// <summary>
+        /// Called when at max speed.
+        /// </summary>
+        private void OnStopAccelerating()
+        {
+            isAccelerating = false;
+            timeSinceAccelerating = 0;
+        }
+        
+        private void OnStartDecelerating()
+        {
+            isDecelerating = true;
+            timeSinceDecelerating = 0;
+        }
+
+        private void OnDecelerate()
+        {
+            timeSinceDecelerating += Time.deltaTime;
+        }
+        
+        private void onStopDecelerating()
+        {
+            isDecelerating = false;
+            timeSinceDecelerating = 0;
+        }
+        
+        private void Move()
+        {
+            var targetPos = GetTargetPosition();
+            if (targetPos == null)
+            {
+                Despawn();
+                return;
+            }
+
+            if (!isMoving)
+                OnStartMoving();
+            
+            if (isAccelerating && timeSinceAccelerating < accelerationDuration)
+                OnAccelerate();
+
+            if (isDecelerating && timeSinceDecelerating < decelerationDuration)
+                OnDecelerate();
+            
+            float accelerationFactor = isAccelerating ? Mathf.Clamp01(timeSinceAccelerating / accelerationDuration) : 1;
+            float decelerationFactor = isDecelerating ? Mathf.Clamp01(timeSinceDecelerating / decelerationDuration) : 1;
+            speed = maxSpeed * (accelerationFactor * decelerationFactor);
+            
+            var (targetPosition, targetRotation) = targetPos.Value;
+
+            Vector3 directionToTarget = targetPosition - transform.position;
+            Quaternion targetRotationFinal = Quaternion.LookRotation(directionToTarget); //face towards the target position
+            
+            Vector3 targetVelocity = transform.forward * speed; //car always moves forward
+            rigidbody.velocity = targetVelocity;
+
+            float speedFactor = Mathf.Clamp01(speed / maxSpeed);
+            float finalTurnSpeed = turnSpeed * speedFactor;
+            rigidbody.MoveRotation(Quaternion.Slerp(rigidbody.rotation, targetRotationFinal, finalTurnSpeed * Time.deltaTime));
+        }
+        
+        private void RotateWheels()
+        {
+            foreach (Transform wheel in wheels)
+            {
+                wheel.Rotate(Vector3.up, wheelRotateSpeed * speed * Time.deltaTime, Space.Self);
+            }
+        }
+        
+        private void TurnFrontWheels()
+        {
+            const int maxTurningDistance = 6; //the number of spline samples
+            
+            float speedFactor = Mathf.Clamp01(speed / maxSpeed);
+
+            int distanceAhead = Mathf.RoundToInt(Mathf.Lerp(movementTargetDistance, maxTurningDistance, speedFactor));
+            
+            SplineSample? desiredSplineSample = GetSplineSampleAhead(distanceAhead);
+            if (desiredSplineSample == null)
+                return; //no more chunks
+            
+            var (position, rotation) = currentChunk.TrafficManager.GetLanePosition(desiredSplineSample.Value, currentLaneDistance);
+            Vector3 directionAhead = (position - transform.position).normalized;
+            
+            foreach (Transform wheel in frontWheels)
+            {
+                const float wheelTurnSpeed = 1f;
+                Quaternion targetRotation = Quaternion.LookRotation(directionAhead, wheel.transform.up);
+                wheel.rotation = Quaternion.Slerp(wheel.rotation, targetRotation, Time.deltaTime * wheelTurnSpeed);
+            }
+        }
+
+        /// <summary>
+        /// Get the next desired position and rotation relative to the sample on the next chunk's spline.
+        /// </summary>
+        /// <returns>The spline sample's position and rotation, or null if no more loaded chunks in the desired direction.</returns>
+        private (Vector3, Quaternion)? GetTargetPosition()
+        {
+            if (currentChunk == null)
+                return null;
+            
+            if (currentChunk.TrafficManager == null)
+            {
+                Debug.LogWarning($"A traffic car is on the chunk {currentChunk.gameObject.name}, but it doesn't have a traffic manager.");
+                return null;
+            }
+
+            SplineSample? desiredSplineSample = GetSplineSampleAhead(movementTargetDistance);
+            if (desiredSplineSample == null)
+                return null; //no more chunks loaded
+            
+            var (position, rotation) = currentChunk.TrafficManager.GetLanePosition(desiredSplineSample.Value, currentLaneDistance);
+
+            return (position, rotation);
+        }
+        
         /// <summary>
         /// Gets the spline sample that is sampleDistance away from the closest sample.
         /// </summary>
@@ -233,83 +387,6 @@ namespace Gumball
 
             return currentChunk.SplineSamples[desiredSplineSampleIndex];
         }
-        
-        private void RotateWheels()
-        {
-            float speed = transform.InverseTransformDirection(rigidbody.velocity).z;
-
-            foreach (Transform wheel in wheels)
-            {
-                wheel.Rotate(Vector3.up, wheelRotateSpeed * speed * Time.deltaTime, Space.Self);
-            }
-        }
-
-        private void TurnFrontWheels()
-        {
-            const int turningDistance = 5; //the number of spline samples
-
-            SplineSample? desiredSplineSample = GetSplineSampleAhead(turningDistance);
-            if (desiredSplineSample == null)
-                return; //no more chunks
-            
-            var (position, rotation) = currentChunk.TrafficManager.GetLanePosition(desiredSplineSample.Value, currentLaneDistance);
-            Vector3 directionAhead = (position - transform.position).normalized;
-            float angle = Vector2.SignedAngle(transform.forward.FlattenAsVector2(), directionAhead.FlattenAsVector2());
-
-            foreach (Transform wheel in frontWheels)
-            {
-                if (debug)
-                {
-                    GlobalLoggers.TrafficLogger.Log("Angle is " + angle);
-                    Debug.DrawRay(wheel.transform.position, transform.forward.Flatten() * 5, Color.blue);
-                    Debug.DrawRay(wheel.transform.position, directionAhead.Flatten() * 5, Color.red);
-                }
-                
-                wheel.LookAt(wheel.transform.position + directionAhead, wheel.transform.up);
-            }
-        }
-        
-        private void Move()
-        {
-            var targetPos = GetTargetPosition();
-            if (targetPos == null)
-            {
-                Despawn();
-                return;
-            }
-            
-            var (targetPosition, targetRotation) = targetPos.Value;
-            
-            Vector3 direction = (targetPosition - transform.position).normalized;
-            Vector3 targetVelocity = direction * moveSpeed;
-            rigidbody.velocity = Vector3.Lerp(rigidbody.velocity, targetVelocity, moveSpeed * Time.deltaTime);
-
-            rigidbody.MoveRotation(Quaternion.Slerp(rigidbody.rotation, targetRotation, turnSpeed * Time.deltaTime));
-        }
-
-        /// <summary>
-        /// Get the next desired position and rotation relative to the sample on the next chunk's spline.
-        /// </summary>
-        /// <returns>The spline sample's position and rotation, or null if no more loaded chunks in the desired direction.</returns>
-        private (Vector3, Quaternion)? GetTargetPosition()
-        {
-            if (currentChunk == null)
-                return null;
-            
-            if (currentChunk.TrafficManager == null)
-            {
-                Debug.LogWarning($"A traffic car is on the chunk {currentChunk.gameObject.name}, but it doesn't have a traffic manager.");
-                return null;
-            }
-
-            SplineSample? desiredSplineSample = GetSplineSampleAhead(1);
-            if (desiredSplineSample == null)
-                return null; //no more chunks loaded
-            
-            var (position, rotation) = currentChunk.TrafficManager.GetLanePosition(desiredSplineSample.Value, currentLaneDistance);
-
-            return (position, rotation);
-        }
 
         private void TryDelayedUpdate()
         {
@@ -324,6 +401,7 @@ namespace Gumball
 
         private void Despawn()
         {
+            OnStopMoving();
             gameObject.Pool();
         }
 
