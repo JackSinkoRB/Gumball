78745fbdabfcbeabb2486ab752dd042d8a34f1ec
Jack Sinko <jack@roadburngames.com>
2024-02-05 14:39:01 +1030

    Give car's ID for debugging

--- Assets/_Gumball/Runtime/Scripts/Traffic/TrafficCarSpawner.cs
+++ Assets/_Gumball/Runtime/Scripts/Traffic/TrafficCarSpawner.cs
@@ -8,12 +8,22 @@ namespace Gumball
     public class TrafficCarSpawner : Singleton<TrafficCarSpawner>
     {
 
+#if UNITY_EDITOR
+        private int trafficCarID; //a unique identifier for debugging
+#endif
+        
         [SerializeField] private TrafficCar[] trafficCarPrefabs;
 
         public TrafficCar SpawnCar(Chunk chunk, Vector3 position, Quaternion rotation)
         {
             TrafficCar randomCarVariant = trafficCarPrefabs.GetRandom().gameObject.GetSpareOrCreate<TrafficCar>(transform, position, rotation);
             randomCarVariant.Initialise(chunk);
+            
+#if UNITY_EDITOR
+            randomCarVariant.name = $"TrafficCar-{randomCarVariant.gameObject.name}-{trafficCarID}";
+            trafficCarID++;
+#endif
+            
             return randomCarVariant;
         }
 
