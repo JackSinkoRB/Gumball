757ab75199ca22351196e97fc5ecb5302d0e301c
Jack Sinko <jack@roadburngames.com>
2024-02-15 09:19:05 +1030

    Add game session logic

---
+++ Assets/_Gumball/Runtime/Scripts/GameSession/GameSession.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gumball
+{
+    [Serializable]
+    public abstract class GameSession : ScriptableObject
+    {
+
+        [SerializeField] private MapData mapToUse;
+
+    }
+}
