ed350a7186ba640d4e4b41b7105a6467b478354e
Jack Sinko <jack@roadburngames.com>
2024-04-09 11:24:55 +0930

    Set timer label color to red when under X seconds

--- Assets/_Gumball/Runtime/Scripts/UI/Menus/TimedSessionPanel.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/TimedSessionPanel.cs
@@ -9,12 +9,27 @@ namespace Gumball
     public class TimedSessionPanel : AnimatedPanel
     {
 
+        /// <summary>
+        /// The time (in seconds) remaining for it to be considered time nearly out.
+        /// </summary>
+        private const float timeNearlyOutTime = 10;
+        
         [SerializeField] private TextMeshProUGUI timerLabel;
+        [SerializeField] private Color timeNearlyOutTimerLabelColor = Color.red;
         [SerializeField] private TextMeshProUGUI distanceLabel;
 
+        private Color defaultTimerLabelColor;
+        
         private TimedGameSession currentSession => (TimedGameSession) GameSessionManager.Instance.CurrentSession;
         private SplineTravelDistanceCalculator playersDistanceCalculator => WarehouseManager.Instance.CurrentCar.GetComponent<SplineTravelDistanceCalculator>();
-        
+
+        protected override void Initialise()
+        {
+            base.Initialise();
+            
+            defaultTimerLabelColor = timerLabel.color;
+        }
+
         private void LateUpdate()
         {
             GameSession session = GameSessionManager.Instance.CurrentSession;
@@ -35,6 +50,8 @@ namespace Gumball
         {
             const float timeRemainingForMs = 10; //if timer goes below this value, show the milliseconds
             timerLabel.text = TimeSpan.FromSeconds(currentSession.TimeRemainingSeconds).ToPrettyString(currentSession.TimeRemainingSeconds < timeRemainingForMs, precise: false);
+
+            timerLabel.color = currentSession.TimeRemainingSeconds > timeNearlyOutTime ? defaultTimerLabelColor : timeNearlyOutTimerLabelColor;
         }
 
     }
