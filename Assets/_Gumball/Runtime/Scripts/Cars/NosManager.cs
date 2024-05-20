9efb1982de6100bfdfd909f2cd521901fa3dfccb
Jack Sinko <jack@roadburngames.com>
2024-05-20 18:25:56 +0930

    Add peak torque while NOS is activated

--- Assets/_Gumball/Runtime/Scripts/Cars/NosManager.cs
+++ Assets/_Gumball/Runtime/Scripts/Cars/NosManager.cs
@@ -10,16 +10,16 @@ namespace Gumball
     {
 
         public const float MinPercentToActivate = 0.1f;
-        
-        /// <summary>
-        /// How long (in seconds) does a full tank of NOS last?
-        /// </summary>
-        private const float depletionRate = 3; //TODO: might want to make this upgradable (use save data)
-        /// <summary>
-        /// How long (in seconds) does it take to regenerate a full tank of NOS?
-        /// </summary>
-        private const float fillRate = 30; //TODO: might want to make this upgradable (use save data)
 
+        [Tooltip("How long (in seconds) does a full tank of NOS last?")]
+        [SerializeField] private float depletionRate = 5; //TODO: might want to make this upgradable (use save data)
+        
+        [Tooltip("How long (in seconds) does it take to regenerate a full tank of NOS?")]
+        [SerializeField] private float fillRate = 30; //TODO: might want to make this upgradable (use save data)
+
+        [Tooltip("The multiplier to apply to the cars torque when NOS is activated.")]
+        [SerializeField] private float torqueAddition = 1500; //TODO: might want to make this upgradable (use save data)
+        
         [Header("Debugging")]
         [SerializeField, ReadOnly] private AICar carBelongsTo;
         
@@ -36,8 +36,10 @@ namespace Gumball
             if (IsActivated)
             {
                 Deplete();
+                
+                //deactivate if no more NOS
                 if (AvailableNosPercent == 0)
-                    IsActivated = false;
+                    Deactivate();
             }
             else
             {
@@ -62,14 +64,30 @@ namespace Gumball
 
         public void Activate()
         {
+            if (IsActivated)
+            {
+                Debug.LogWarning("Cannot activate NOS as it is already activated.");
+                return;
+            }
+            
             IsActivated = true;
-        }
 
+            carBelongsTo.SetPeakTorque(carBelongsTo.PeakTorque + torqueAddition);
+        }
+        
         public void Deactivate()
         {
+            if (!IsActivated)
+            {
+                Debug.LogWarning("Cannot deactivate NOS as it is not activated.");
+                return;
+            }
+            
             IsActivated = false;
+            
+            carBelongsTo.SetPeakTorque(carBelongsTo.PeakTorque - torqueAddition);
         }
-
+        
         private void Deplete()
         {
             float percent = Time.deltaTime / depletionRate;
