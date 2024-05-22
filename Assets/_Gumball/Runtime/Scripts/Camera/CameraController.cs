562b9ef43443b965c6edac24e5852e76713315b0
Jack Sinko <jack@roadburngames.com>
2024-05-22 12:44:34 +0930

    Update camera logic to follow cars velocity direction and rotate by pivoting around the car

--- Assets/_Gumball/Runtime/Scripts/Camera/CameraController.cs
+++ Assets/_Gumball/Runtime/Scripts/Camera/CameraController.cs
@@ -85,6 +85,43 @@ namespace Gumball
             CurrentState.SnapToTarget();
         }
 
+        
+        //TODO: move this to CameraState - all the values should be relative to the state
+        [SerializeField] private float positionLerpSpeed = 6;
+        [SerializeField] private Transform rotationPivot;
+        [SerializeField] private Vector3 offset = new(0, 2, -6);
+        [SerializeField] private Vector3 lookAtOffset = new(0, 1, 0);
+
+        private void LerpPosition()
+        {
+            //TODO: move this to CameraState - all the values should be relative to the state
+            
+            // - should always be looking at the car centre (plus some offset for height)
+            // - should always be the same distance away from the car centre
+            // - should always have the same height above the car centre
+            // - interpolate from current position to desired position (X distance behind car, X distance above car
+            // - y (look) rotation is slightly interpolated
+
+            //TODO: move to property in CameraState
+            AICar playerCar = WarehouseManager.Instance.CurrentCar;
+
+            //TODO: cleanup - move to separate method
+            //set the position to match the desired offset - but keep height relative to the car
+            float heightRelativeToCar = playerCar.transform.TransformPoint(offset).y;
+            transform.position = playerCar.transform.position.SetY(heightRelativeToCar) + new Vector3(offset.x, 0, offset.z);
+            
+            //TODO: cleanup - move to separate method
+            //rotate the pivot around the car with some interpolation:
+            //get the angle between the players velocity forward and pivot forward in the XZ plane
+            // - if velocity is close to 0, use the players forward
+            const float velocityTolerance = 0.1f;
+            Vector3 targetDirection = playerCar.Rigidbody.velocity.sqrMagnitude < velocityTolerance ? playerCar.transform.forward : playerCar.Rigidbody.velocity;
+            float angleForDesiredRotation = Vector2.SignedAngle(rotationPivot.forward.FlattenAsVector2(), targetDirection.FlattenAsVector2());
+            rotationPivot.RotateAround(playerCar.transform.position + lookAtOffset, Vector3.up, -angleForDesiredRotation * Time.deltaTime * positionLerpSpeed);
+            
+            rotationPivot.LookAt(playerCar.transform.position + lookAtOffset);
+        }
+        
         private void SetPositionAndRotation()
         {
             if (CurrentState == null)
@@ -94,8 +131,8 @@ namespace Gumball
             if (currentTransition == null)
             {
                 var (position, rotation) = CurrentState.Calculate();
-                transform.position = position;
-                transform.rotation = rotation;
+                
+                LerpPosition();
             }
             else
             {
