cf56b655d95f733e330a72e49e9e3af873c082d2
Jack Sinko <jack@roadburngames.com>
2024-05-30 16:37:40 +0930

    Remove old UI and update gear label with R and N

--- Assets/_Gumball/Runtime/Scripts/UI/DrivingHUD/GearLabel.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/DrivingHUD/GearLabel.cs
@@ -14,13 +14,25 @@ namespace Gumball
 
         private void LateUpdate()
         {
-            label.text = $"<size=50>{GetGearAsFriendlyString(WarehouseManager.Instance.CurrentCar.CurrentGear)}</size>\nGear";
+            if (!WarehouseManager.HasLoaded || WarehouseManager.Instance.CurrentCar == null)
+            {
+                label.text = GetFinalString("N");
+                return;
+            }
+
+            label.text = GetFinalString(GetGearAsFriendlyString(WarehouseManager.Instance.CurrentCar.CurrentGear));
+        }
+
+        private string GetFinalString(string gearString)
+        {
+            return $"<size=50>{gearString}</size>\nGear";
         }
         
         private string GetGearAsFriendlyString(int gear)
         {
             return gear switch
             {
+                0 => "R",
                 1 => "1st",
                 2 => "2nd",
                 3 => "3rd",
