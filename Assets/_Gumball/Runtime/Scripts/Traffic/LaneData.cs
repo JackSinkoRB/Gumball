6498d52280b4c4c612a14153ab16431292d3a4b4
Jack Sinko <jack@roadburngames.com>
2024-06-12 09:08:33 +0930

    Add LaneData

---
+++ Assets/_Gumball/Runtime/Scripts/Traffic/LaneData.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gumball
+{
+    public readonly struct LaneData
+    {
+            
+        public float LaneOffset { get; }
+        public CustomDrivingPath CustomLane { get; }
+        public float RandomLaneDistance { get; }
+        public (Vector3, Quaternion) LanePosition { get; }
+            
+        public bool IsCustomLane => CustomLane != null;
+            
+        public LaneData(float laneOffset, CustomDrivingPath customLane, float randomLaneDistance, (Vector3, Quaternion) lanePosition)
+        {
+            LaneOffset = laneOffset;
+            CustomLane = customLane;
+            RandomLaneDistance = randomLaneDistance;
+            LanePosition = lanePosition;
+        }
+            
+    }
+}
