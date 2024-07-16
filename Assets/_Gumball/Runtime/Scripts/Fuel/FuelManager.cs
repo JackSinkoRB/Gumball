637b1512fbd3853fe97c823c25f5b790c1eff8e7
Jack Sinko <jack@roadburngames.com>
2024-07-16 13:10:34 +0930

    Add PersistentCooldown, time offset to TimeUtils, and give 1 fuel every 15 minutes

--- Assets/_Gumball/Runtime/Scripts/Fuel/FuelManager.cs
+++ Assets/_Gumball/Runtime/Scripts/Fuel/FuelManager.cs
@@ -8,9 +8,16 @@ namespace Gumball
     {
         
         public const int MaxFuel = 10;
-
+        
+        /// <summary>
+        /// How often does the player get given a fuel automatically?
+        /// </summary>
+        public const int MinutesBetweenFuelRegenerate = 15;
+        
         public delegate void OnFuelChangeDelegate(int previousFuel, int newFuel);
         public static OnFuelChangeDelegate onFuelChange;
+
+        public static PersistentCooldown RegenerateCycle { get; private set; }
         
         public static int CurrentFuel
         {
@@ -18,6 +25,14 @@ namespace Gumball
             private set => DataManager.Player.Set("Fuel.Current", value);
         }
 
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        private static void RuntimeInitialise()
+        {
+            RegenerateCycle = new PersistentCooldown("FuelRegenerateCycle", MinutesBetweenFuelRegenerate * TimeUtils.SecondsInAMinute);
+            RegenerateCycle.onCycleComplete += RegenerateFuel;
+            RegenerateCycle.Play();
+        }
+        
         public static bool HasFuel(int amount = 1)
         {
             return CurrentFuel >= amount;
@@ -30,9 +45,6 @@ namespace Gumball
         
         public static void SetFuel(int amount)
         {
-            if (CurrentFuel == amount)
-                return; //already has this amount
-            
             if (amount > MaxFuel)
             {
                 Debug.LogError("Cannot set fuel above the maximum.");
@@ -44,11 +56,21 @@ namespace Gumball
                 Debug.LogError("Cannot set fuel below 0.");
                 amount = 0;
             }
-
+            
+            if (CurrentFuel == amount)
+                return; //already has this amount
+            
             int previousFuel = CurrentFuel;
             CurrentFuel = amount;
             
             onFuelChange?.Invoke(previousFuel, amount);
+
+            bool wasMaxFuel = previousFuel == MaxFuel && CurrentFuel < MaxFuel;
+            if (wasMaxFuel)
+            {
+                //restart the regenerate timer
+                RegenerateCycle.Restart();
+            }
         }
         
         public static void AddFuel(int amount = 1)
@@ -61,5 +83,13 @@ namespace Gumball
             SetFuel(CurrentFuel - amount);
         }
         
+        private static void RegenerateFuel()
+        {
+            if (CurrentFuel == MaxFuel)
+                return;
+            
+            AddFuel();
+        }
+
     }
 }
