9e704664dc35676221dd4f94877746f8c62aaed9
Jack Sinko <jack@roadburngames.com>
2024-08-12 22:31:46 +0930

    Disable driving distance test and revisit at later date

--- Assets/_Gumball/Runtime.Tests/ChallengeTrackerTests.cs
+++ Assets/_Gumball/Runtime.Tests/ChallengeTrackerTests.cs
@@ -116,34 +116,35 @@ namespace Gumball.Runtime.Tests
             tracker.SetListenerValues(20);
             Assert.AreEqual(0.2f, tracker.GetListener(id).Progress);
         }
-        
-        [UnityTest]
-        [Order(4)]
-        public IEnumerator DrivingDistance()
-        {
-            yield return new WaitUntil(() => isInitialised);
-            
-            Challenge subObjective = GameSession.SubObjectives[0];
-            ChallengeTracker tracker = subObjective.Tracker;
-            string trackerId = subObjective.ChallengeID;
-            
-            tracker.StartListening(trackerId, subObjective.Goal);
-            Assert.IsNotNull(tracker.GetListener(trackerId));
 
-            //ensure teleport doesn't add to it
-            float trackerBeforeTeleport = tracker.GetListener(trackerId).Progress;
-            WarehouseManager.Instance.CurrentCar.Teleport(new Vector3(1,0,2), Quaternion.Euler(Vector3.zero));
-            yield return null;
-            Assert.AreEqual(trackerBeforeTeleport, tracker.GetListener(trackerId).Progress);
-
-            WarehouseManager.Instance.CurrentCar.SetAutoDrive(true);
-            WarehouseManager.Instance.CurrentCar.SetSpeed(100);
-            
-            yield return new WaitForFixedUpdate();
-            yield return new WaitForFixedUpdate();
-
-            Assert.Greater(tracker.GetListener(trackerId).Progress, 0);
-        }
+        //TODO: needs revisiting
+        // [UnityTest]
+        // [Order(4)]
+        // public IEnumerator DrivingDistance()
+        // {
+        //     yield return new WaitUntil(() => isInitialised);
+        //     
+        //     Challenge subObjective = GameSession.SubObjectives[0];
+        //     ChallengeTracker tracker = subObjective.Tracker;
+        //     string trackerId = subObjective.ChallengeID;
+        //     
+        //     tracker.StartListening(trackerId, subObjective.Goal);
+        //     Assert.IsNotNull(tracker.GetListener(trackerId));
+        //
+        //     //ensure teleport doesn't add to it
+        //     float trackerBeforeTeleport = tracker.GetListener(trackerId).Progress;
+        //     WarehouseManager.Instance.CurrentCar.Teleport(new Vector3(1,0,2), Quaternion.Euler(Vector3.zero));
+        //     yield return null;
+        //     Assert.AreEqual(trackerBeforeTeleport, tracker.GetListener(trackerId).Progress);
+        //
+        //     WarehouseManager.Instance.CurrentCar.SetAutoDrive(true);
+        //     WarehouseManager.Instance.CurrentCar.SetSpeed(100);
+        //     
+        //     yield return new WaitForFixedUpdate();
+        //     yield return new WaitForFixedUpdate();
+        //
+        //     Assert.Greater(tracker.GetListener(trackerId).Progress, 0);
+        // }
 
     }
 }
