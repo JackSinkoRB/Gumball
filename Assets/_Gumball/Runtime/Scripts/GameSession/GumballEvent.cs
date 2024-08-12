26becae080afcffed9e73c73b4ed35c988a57297
Jack Sinko <jack@roadburngames.com>
2024-08-12 17:03:44 +0930

    Add rebuild all maps button in GumballEvent

--- Assets/_Gumball/Runtime/Scripts/GameSession/GumballEvent.cs
+++ Assets/_Gumball/Runtime/Scripts/GameSession/GumballEvent.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
@@ -14,5 +15,36 @@ namespace Gumball
 
         public AssetReferenceGameObject[] Maps => maps;
 
+#if UNITY_EDITOR
+        [ButtonMethod]
+        public void RebuildAllMaps()
+        {
+            if (Application.isPlaying)
+                throw new InvalidOperationException("Cannot rebuild during play mode.");
+
+            HashSet<AssetReferenceT<ChunkMap>> chunkMapsToRebuild = new();
+
+            //find all the chunks maps
+            foreach (AssetReferenceGameObject mapReference in maps)
+            {
+                GameSessionMap map = mapReference.editorAsset.GetComponent<GameSessionMap>();
+                foreach (GameSessionNode node in map.Nodes)
+                {
+                    AssetReferenceT<ChunkMap> chunkMapReference = node.GameSession.ChunkMapAssetReference;
+                    chunkMapsToRebuild.Add(chunkMapReference);
+                }
+            }
+
+            //rebuild the data (but only recreate runtime chunks once)
+            foreach (AssetReferenceT<ChunkMap> chunkMap in chunkMapsToRebuild)
+            {
+                chunkMap.editorAsset.RebuildData(false);
+            }
+            
+            //reset the runtime chunk creation tracking
+            ChunkMap.ClearRuntimeChunksCreatedTracking();
+        }
+#endif
+        
     }
 }
