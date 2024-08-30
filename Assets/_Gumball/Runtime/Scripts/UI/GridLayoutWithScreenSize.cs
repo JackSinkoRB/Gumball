9ddcf68778c0e9c6d58f011687898727bf9815d4
Jack Sinko <jack@roadburngames.com>
2024-08-30 11:04:14 +0930

    Add GridLayoutWithScreenSize component

--- Assets/_Gumball/Runtime/Scripts/UI/GridLayoutWithScreenSize.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/GridLayoutWithScreenSize.cs
@@ -17,18 +17,71 @@ namespace Gumball
         }
 
         [SerializeField] private LayoutDirection layoutDirection;
-        [SerializeField, ConditionalField(nameof(layoutDirection), true)] private int numberOfColumns = 1;
-        [SerializeField, ConditionalField(nameof(layoutDirection))] private int numberOfRows = 1;
+        [SerializeField, ReadOnly(nameof(layoutDirection))] private int numberOfColumns = 1;
+        [SerializeField, ReadOnly(nameof(layoutDirection), true)] private int numberOfRows = 1;
+        
+        [Space(5)]
+        [Tooltip("The size of the elements as a percentage of the rect size.")]
+        [SerializeField, Range(0.01f, 1)] private float elementSizeAsPercent;
 
+        private RectTransform rectTransform => transform as RectTransform;
+        
         private void OnValidate()
         {
             Resize();
         }
 
-        [ButtonMethod]
+        private void Update()
+        {
+            if (!Application.isPlaying)
+                Resize();
+        }
+        
         public void Resize()
         {
+            //changing the size of the elements
+
+            float rectWidth = rectTransform.rect.width;
+            float rectHeight = rectTransform.rect.height;
+
+            if (layoutDirection == LayoutDirection.HORIZONTAL)
+            {
+                if (numberOfColumns == 0)
+                    return;
+                numberOfRows = Mathf.CeilToInt((float)transform.childCount / numberOfColumns);
+            }
+            else
+            {
+                if (numberOfRows == 0)
+                    return;
+                numberOfColumns = Mathf.CeilToInt((float)transform.childCount / numberOfRows);
+            }
+
+            float elementSize = numberOfColumns > numberOfRows ? rectWidth * elementSizeAsPercent : rectHeight * elementSizeAsPercent;
+
+            Vector2 spacing = new Vector2(
+                (rectWidth - (elementSize * numberOfColumns)) / (numberOfColumns - 1), 
+                (rectHeight - (elementSize * numberOfRows)) / (numberOfRows - 1));
             
+            int count = 0;
+            foreach (RectTransform child in transform)
+            {
+                child.anchorMin = new Vector2(0, 1);
+                child.anchorMin = new Vector2(0, 1);
+                child.pivot = new Vector2(0, 1);
+                
+                child.sizeDelta = Vector2.one * elementSize;
+
+                int column = count % numberOfColumns;
+                int row = Mathf.FloorToInt((float)count / numberOfColumns);
+
+                float xPos = (spacing.x * column) + (column * elementSize);
+                float yPos = -(spacing.y * row) - (row * elementSize);
+                
+                child.anchoredPosition = new Vector2(xPos, yPos);
+
+                count++;
+            }
         }
         
     }
