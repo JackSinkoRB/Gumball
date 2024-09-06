7ab0f938c2381cc795a628bb8c9b00d457b8525e
Jack Sinko <jack@roadburngames.com>
2024-09-06 13:17:38 +0930

    Setup timed session end panel

--- Assets/_Gumball/Runtime/Scripts/UI/Menus/SessionPanels/TimedSessionEndPanel.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/SessionPanels/TimedSessionEndPanel.cs
@@ -7,7 +7,15 @@ namespace Gumball
     public class TimedSessionEndPanel : SessionEndPanel
     {
         
-        
+        private TimedGameSession timedSession => GameSessionManager.Instance.CurrentSession as TimedGameSession;
+
+        protected override void OnShow()
+        {
+            base.OnShow();
+            
+            SetVictory(timedSession.TimeRemainingSeconds > 0);
+            ShowPosition(false);
+        }
         
     }
 }
