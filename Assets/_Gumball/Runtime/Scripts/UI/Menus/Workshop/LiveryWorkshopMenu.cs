f3492f0a3008ec39beb3930791aba393969870ef
Jack Sinko <jack@roadburngames.com>
2024-09-23 15:25:12 +0930

    Setup customisation menu buttons

---
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/Workshop/LiveryWorkshopMenu.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gumball
+{
+    public class LiveryWorkshopMenu : WorkshopSubMenu
+    {
+        
+        
+        
+    }
+}
