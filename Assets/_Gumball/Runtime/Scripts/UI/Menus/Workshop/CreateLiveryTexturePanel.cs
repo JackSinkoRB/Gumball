6a0b84d3a2273e0c73217c978324c5284b9613b9
Jack Sinko <jack@roadburngames.com>
2024-10-17 21:28:51 +1030

    Choose position on car after selecting a decal to add

--- Assets/_Gumball/Runtime/Scripts/UI/Menus/Workshop/CreateLiveryTexturePanel.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/Workshop/CreateLiveryTexturePanel.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Gumball.Editor;
 using MyBox;
 using UnityEngine;
 
@@ -45,6 +46,19 @@ namespace Gumball
             Hide();
             
             LiveDecal decal = DecalEditor.Instance.CreateLiveDecal(headerFilter.CurrentSelected, selectedOption.DecalTexture);
+            
+            //move to the car position
+            RaycastHit[] results = new RaycastHit[1];
+            int hits = Physics.BoxCastNonAlloc(Camera.main.transform.position, new Vector3(0.1f, 10, 1), (WarehouseManager.Instance.CurrentCar.transform.position - Camera.main.transform.position).normalized,  results, Quaternion.LookRotation((WarehouseManager.Instance.CurrentCar.transform.position - Camera.main.transform.position).normalized), 1000, LayersAndTags.GetLayerMaskFromLayer(LayersAndTags.Layer.PaintableMesh));
+            if (hits > 0)
+            {
+                RaycastHit hit = results[0];
+                Quaternion rotation = Quaternion.LookRotation(Camera.main.transform.forward - hit.normal, Vector3.up);
+                decal.UpdatePosition(DecalEditor.Instance.CurrentCar.transform.InverseTransformPoint(hit.point), hit.normal, rotation);
+            }
+            
+            decal.SetValid(hits > 0);
+            
             DecalStateManager.LogStateChange(new DecalStateManager.CreateStateChange(decal));
             DecalEditor.Instance.SelectLiveDecal(decal);
         }
