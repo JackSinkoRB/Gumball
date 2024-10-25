8bb44d61e77c178506d7d736a772539f5a754511
Jack Sinko <jack@roadburngames.com>
2024-10-25 21:14:01 +1030

    Set min value of 1 for blueprint reward

--- Assets/_Gumball/Runtime/Scripts/Blueprints/BlueprintReward.cs
+++ Assets/_Gumball/Runtime/Scripts/Blueprints/BlueprintReward.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using MyBox;
 using UnityEngine;
 
 namespace Gumball
@@ -10,7 +11,7 @@ namespace Gumball
     {
             
         [SerializeField] private int carIndex;
-        [SerializeField] private int blueprints;
+        [SerializeField, MinValue(1)] private int blueprints;
 
         public int CarIndex => carIndex;
         public int Blueprints => blueprints;
