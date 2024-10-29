f89fb52b711bbb3c9361aa31a2132cf0ddcf2fd8
Jack Sinko <jack@roadburngames.com>
2024-10-29 17:52:51 +1030

    Add ambience sound to traffic cars for pass by effect

---
+++ Assets/_Gumball/Runtime/Scripts/Audio/AmbienceCarAudio.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MyBox;
+using UnityEngine;
+
+namespace Gumball
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AmbienceCarAudio : MonoBehaviour
+    {
+
+        [SerializeField] private float maxVolume = 1;
+        [SerializeField] private MinMaxFloat speedRangeKmh = new(0,30);
+
+        private AICar carBelongsTo;
+
+        private AudioSource source => GetComponent<AudioSource>();
+        
+        private void OnEnable()
+        {
+            carBelongsTo = transform.GetComponentInAllParents<AICar>();
+        }
+
+        private void LateUpdate()
+        {
+            AdjustVolumeBasedOnSpeed();
+        }
+
+        private void AdjustVolumeBasedOnSpeed()
+        {
+            float speedAsPercent = Mathf.Clamp01((carBelongsTo.SpeedKmh - speedRangeKmh.Min) / speedRangeKmh.Difference);
+            source.volume = speedAsPercent * maxVolume;
+        }
+        
+    }
+}
