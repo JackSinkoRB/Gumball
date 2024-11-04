a91b52d6e1ebc975ff7e9da5f7e3bed9522a6bce
Jack Sinko <jack@roadburngames.com>
2024-11-04 19:41:08 +1030

    Major reward to use dynamic height based on screen size

--- Assets/_Gumball/Runtime/Scripts/UI/Menus/Challenges/DailyLoginChallengesSubMenu.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/Challenges/DailyLoginChallengesSubMenu.cs
@@ -58,12 +58,19 @@ namespace Gumball
             //set the size as the same as the minor rewards grid to set inline
             majorRewardsHolder.sizeDelta = majorRewardsHolder.sizeDelta.SetY(minorRewardsGrid.GetComponent<RectTransform>().sizeDelta.y);
 
-            //copy the minor reward heights
+            //copy the minor reward positions
             majorReward1.GetComponent<RectTransform>().anchoredPosition = majorReward1.GetComponent<RectTransform>().anchoredPosition.SetY(minorRewardNodes[0].GetComponent<RectTransform>().anchoredPosition.y);
             majorReward2.GetComponent<RectTransform>().anchoredPosition = majorReward2.GetComponent<RectTransform>().anchoredPosition.SetY(minorRewardNodes[7].GetComponent<RectTransform>().anchoredPosition.y);
             majorReward3.GetComponent<RectTransform>().anchoredPosition = majorReward3.GetComponent<RectTransform>().anchoredPosition.SetY(minorRewardNodes[14].GetComponent<RectTransform>().anchoredPosition.y);
             majorReward4.GetComponent<RectTransform>().anchoredPosition = majorReward4.GetComponent<RectTransform>().anchoredPosition.SetY(minorRewardNodes[21].GetComponent<RectTransform>().anchoredPosition.y);
             
+            //set the heights
+            float sizeY = minorRewardNodes[0].GetComponent<RectTransform>().sizeDelta.y; //all nodes have same height
+            majorReward1.GetComponent<RectTransform>().sizeDelta = majorReward1.GetComponent<RectTransform>().sizeDelta.SetY(sizeY);
+            majorReward2.GetComponent<RectTransform>().sizeDelta = majorReward2.GetComponent<RectTransform>().sizeDelta.SetY(sizeY);
+            majorReward3.GetComponent<RectTransform>().sizeDelta = majorReward3.GetComponent<RectTransform>().sizeDelta.SetY(sizeY);
+            majorReward4.GetComponent<RectTransform>().sizeDelta = majorReward4.GetComponent<RectTransform>().sizeDelta.SetY(sizeY);
+                
             //update the major rewards
             majorReward1.Initialise(7, DailyLoginManager.Instance.CurrentMonth.Weeks[0].MajorReward);
             majorReward2.Initialise(14, DailyLoginManager.Instance.CurrentMonth.Weeks[1].MajorReward);
