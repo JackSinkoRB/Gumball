e8b63d6bb477d7ef3d9ba3155155fc6d7394f9e3
Jack Sinko <jack@roadburngames.com>
2024-11-06 00:39:52 +1030

    Update challenges button on main menu panel and add notification if day node is ready

--- Assets/_Gumball/Runtime/Scripts/UI/Menus/MainMenuPanel.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/Menus/MainMenuPanel.cs
@@ -7,7 +7,16 @@ namespace Gumball
 {
     public class MainMenuPanel : AnimatedPanel
     {
+
+        [SerializeField] private Transform challengesNotification;
         
+        protected override void OnShow()
+        {
+            base.OnShow();
+
+            RefreshChallengesNotification();
+        }
+
         public void OnClickChallengesButton()
         {
             PanelManager.GetPanel<ChallengesPanel>().Show();
@@ -26,6 +35,13 @@ namespace Gumball
         public void OnClickStoreButton()
         {
             PanelManager.GetPanel<StorePanel>().Show();
+        }
+
+        private void RefreshChallengesNotification()
+        {
+            int currentDayNumber = DailyLoginManager.Instance.GetCurrentDayNumber();
+            bool isDayWaiting = DailyLoginManager.Instance.IsDayReady(currentDayNumber) && !DailyLoginManager.Instance.IsDayClaimed(currentDayNumber);
+            challengesNotification.gameObject.SetActive(isDayWaiting);
         }
         
     }
