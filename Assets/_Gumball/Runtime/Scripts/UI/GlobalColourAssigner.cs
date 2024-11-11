8b677ed37704fe08ba01d75dc612fe6a71c0ca15
Jack Sinko <jack@roadburngames.com>
2024-11-11 12:00:13 +1030

    Add gradient logic to GlobalColourAssigner

--- Assets/_Gumball/Runtime/Scripts/UI/GlobalColourAssigner.cs
+++ Assets/_Gumball/Runtime/Scripts/UI/GlobalColourAssigner.cs
@@ -3,27 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using MyBox;
 using TMPro;
+using UIGradientUtility;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Gumball
 {
-    public class GlobalColourAssigner : MonoBehaviour
+    public class GlobalColourAssigner : BaseMeshEffect
     {
 
         [SerializeField] private GlobalColourPalette.ColourCode colourCode;
 
+        [Header("Gradient")]
+        [SerializeField] private bool useGradient;
+        [SerializeField, ConditionalField(nameof(useGradient))] private GlobalColourPalette.ColourCode gradientColor;
+        [SerializeField, ConditionalField(nameof(useGradient)), Range(-180f, 180f)] private float gradientAngle;
+        [SerializeField, ConditionalField(nameof(useGradient))] private bool gradientIgnoreRatio = true;
+
         private Image image => GetComponent<Image>();
         private TextMeshProUGUI label => GetComponent<TextMeshProUGUI>();
 
-        private void OnEnable()
+        protected override void OnEnable()
         {
+            base.OnEnable();
+            
             this.PerformAfterTrue(() => GlobalColourPalette.HasLoaded, UpdateColors);
+        }
+        
+        public override void ModifyMesh(VertexHelper vertexHelper)
+        {
+            if (enabled && useGradient)
+                RenderGradient(vertexHelper);
         }
 
 #if UNITY_EDITOR
-        private void OnValidate()
+        protected override void OnValidate()
         {
+            base.OnValidate();
+            
             if (!GlobalColourPalette.HasLoaded)
                 GlobalColourPalette.LoadInstanceSync();
             UpdateColors();
@@ -50,6 +67,28 @@ namespace Gumball
 
             if (label != null)
                 label.color = globalColour.WithAlphaSetTo(label.color.a);;
+        }
+
+        private void RenderGradient(VertexHelper vertexHelper)
+        {
+            Rect rect = graphic.rectTransform.rect;
+            Vector2 dir = UIGradientUtils.RotationDir(gradientAngle);
+
+            if (!gradientIgnoreRatio)
+                dir = UIGradientUtils.CompensateAspectRatio(rect, dir);
+
+            UIGradientUtils.Matrix2x3 localPositionMatrix = UIGradientUtils.LocalPositionMatrix(rect, dir);
+
+            Color color = GlobalColourPalette.Instance.GetGlobalColor(gradientColor);
+            
+            UIVertex vertex = default(UIVertex);
+            for (int i = 0; i < vertexHelper.currentVertCount; i++)
+            {
+                vertexHelper.PopulateUIVertex (ref vertex, i);
+                Vector2 localPosition = localPositionMatrix * vertex.position;
+                vertex.color *= Color.Lerp(color, Color.white, localPosition.y);
+                vertexHelper.SetUIVertex (vertex, i);
+            }
         }
         
     }
